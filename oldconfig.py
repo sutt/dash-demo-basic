fb8d39d7c639a014c25a48d9850068aae053e572
Will Sutton <wsutton17@gmail.com>
2023-05-09 13:16:53 -0400

    basecamp for demo number 1

--- oldconfig.py
+++
@@ -1,8 +0,0 @@
-# Some other example server values are
-# server = 'localhost\sqlexpress' # for a named instance
-# server = 'myserver,port' # to specify an alternate port
-database = "pima"
-table = "dbo.diabetes"
-username = "fakeuser"
-password  = "fakepassword"
-server = "gen10-dbc-apr2021-azsql.database.windows.net"
